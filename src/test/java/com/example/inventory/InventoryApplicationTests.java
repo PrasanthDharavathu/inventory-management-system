d9529ba442229cdbf9bee5b347449c55b064c737
Prasanth <Prasanthdharavathu@gmail.com>
2026-06-02 12:36:17 -0500

    Set H2 datasource properties for context load test

--- src/test/java/com/example/inventory/InventoryApplicationTests.java
+++ src/test/java/com/example/inventory/InventoryApplicationTests.java
@@ -2,14 +2,18 @@ package com.example.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class InventoryApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
